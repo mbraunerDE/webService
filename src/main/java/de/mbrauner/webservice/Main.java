1fce07e59f4e1b633cba093a60a979fc00adacbe
mbraunrDE <github@mbrauner.de>
2016-09-02 20:50:29 +0200

    make listen port changeable

--- src/main/java/de/mbrauner/webservice/Main.java
+++ src/main/java/de/mbrauner/webservice/Main.java
@@ -9,10 +9,14 @@ public class Main {
 	public static final int JETTYPORT = 80;
 
 	public static void main(String[] args) {
-		port(JETTYPORT);
+		if( args != null && args.length == 1){
+			port(Integer.parseInt(args[0]));
+		}else{
+			port(JETTYPORT);
+		}
 		staticFileLocation("/public");
 
-		get("/", (request, response) -> { // Status page
+		get("/", (request, response) -> {
 			response.redirect("/index.html");
 			return null;
 		});
